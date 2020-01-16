a00a5db5f1e3a8c4f8547a65204d368ee14069cf
Joao Juliasz <joaojuliaszdemorais@gmail.com>
2020-01-16 11:14:21 -0300

    Meus pedids e resumo carrinho

--- functions.php
+++ functions.php
@@ -1,19 +1,40 @@
 <?php
+
+use Hcode\Model\Cart;
 use \Hcode\Model\User;
 
-function formatPrice( $vlprice){
+function formatPrice($vlprice)
+{
 
-    if(!$vlprice > 0 ) $vlprice = 0;
+    if (!$vlprice > 0) $vlprice = 0;
     return number_format($vlprice, 2, ",", ".");
 }
 
-function checkLogin($inadmin = true){
+function checkLogin($inadmin = true)
+{
     return User::checkLogin($inadmin);
 }
 
-function getUserName(){
+function getUserName()
+{
 
     $user = User::getFromSession();
 
-    return $user->getdesperson();   
+    return $user->getdesperson();
+}
+
+function getCartNrQtd()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+    return $totals['nrqtd'];
+}
+function getCartVlSubTotal()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
 }
--- site.php
+++ site.php
@@ -134,7 +134,7 @@ $app->get('/checkout', function () {
     $address = new Address;
     $cart = Cart::getFromSession();
 
-    if(!isset($_GET['zipcode'])){
+    if (!isset($_GET['zipcode'])) {
         $_GET['zipcode'] = $cart->getdeszipcode();
     }
 
@@ -146,15 +146,15 @@ $app->get('/checkout', function () {
         $cart->getCalculateTotal();
     }
 
-    if(!$address->getdesaddress()) $address->setdesaddress('');
-    if(!$address->getdescomplement()) $address->setdescomplement('');
-    if(!$address->getdesdistrict()) $address->setdesdistrict('');
-    if(!$address->getdescity()) $address->setdescity('');
-    if(!$address->getdesstate()) $address->setdesstate('');
-    if(!$address->getdesstate()) $address->setdesstate('');
-    if(!$address->getdescountry()) $address->setdescountry('');
-    if(!$address->getdeszipcode()) $address->setdeszipcode('');
-    if(!$address->getdesnumber()) $address->setdesnumber('');
+    if (!$address->getdesaddress()) $address->setdesaddress('');
+    if (!$address->getdescomplement()) $address->setdescomplement('');
+    if (!$address->getdesdistrict()) $address->setdesdistrict('');
+    if (!$address->getdescity()) $address->setdescity('');
+    if (!$address->getdesstate()) $address->setdesstate('');
+    if (!$address->getdesstate()) $address->setdesstate('');
+    if (!$address->getdescountry()) $address->setdescountry('');
+    if (!$address->getdeszipcode()) $address->setdeszipcode('');
+    if (!$address->getdesnumber()) $address->setdesnumber('');
 
 
     $page = new Page;
@@ -162,8 +162,8 @@ $app->get('/checkout', function () {
     $page->setTpl('checkout', [
         'cart' => $cart->getValues(),
         'address' => $address->getValues(),
-        'products'=>$cart->getProducts(),
-        'error'=>Cart::getMsgError()
+        'products' => $cart->getProducts(),
+        'error' => Cart::getMsgError()
     ]);
 });
 
@@ -171,32 +171,32 @@ $app->post("/checkout", function () {
 
     User::verifyLogin(false);
 
-    if(!isset($_POST['zipcode']) || $_POST['zipcode'] === ''){
+    if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
         Address::setMsgError("Informe o cep");
         header('Location: /checkout');
     }
 
-    if(!isset($_POST['desaddres']) || $_POST['desaddres'] === ''){
+    if (!isset($_POST['desaddres']) || $_POST['desaddres'] === '') {
         Address::setMsgError("Informe o endereço");
         header('Location: /checkout');
     }
 
-    if(!isset($_POST['desdistrict']) || $_POST['desdistrict'] === ''){
+    if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
         Address::setMsgError("Informe o bairro");
         header('Location: /checkout');
     }
 
-    if(!isset($_POST['descity']) || $_POST['descity'] === ''){
+    if (!isset($_POST['descity']) || $_POST['descity'] === '') {
         Address::setMsgError("Informe a cidade");
         header('Location: /checkout');
     }
 
-    if(!isset($_POST['desstate']) || $_POST['desstate'] === ''){
+    if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
         Address::setMsgError("Informe o estado");
         header('Location: /checkout');
     }
 
-    if(!isset($_POST['descountry']) || $_POST['descountry'] === ''){
+    if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
         Address::setMsgError("Informe o país");
         header('Location: /checkout');
     }
@@ -216,16 +216,16 @@ $app->post("/checkout", function () {
     $total = $cart->getCalculateTotal();
 
     $order = new Order();
-	$order->setData([
-		'idcart'=>$cart->getidcart(),
-		'idaddress'=>$address->getidaddress(),
-		'iduser'=>$user->getiduser(),
-		'idstatus'=>OrderStatus::EM_ABERTO,
-		'vltotal'=>$cart->getvltotal()
-	]);
-	$order->save();
+    $order->setData([
+        'idcart' => $cart->getidcart(),
+        'idaddress' => $address->getidaddress(),
+        'iduser' => $user->getiduser(),
+        'idstatus' => OrderStatus::EM_ABERTO,
+        'vltotal' => $cart->getvltotal()
+    ]);
+    $order->save();
 
-    header("Location: /order/".$order->getidorder());
+    header("Location: /order/" . $order->getidorder());
     exit;
 });
 
@@ -393,73 +393,107 @@ $app->post('/profile', function () {
     User::setSuccess("Dados alterados com sucesso");
     header('Location: /profile');
     exit;
-}); 
+});
 
-$app->get("/order/:idorder", function($idorder){
+$app->get("/order/:idorder", function ($idorder) {
     User::verifyLogin(false);
 
     $order = new Order;
 
-    $order->get((int)$idorder);
-    
+    $order->get((int) $idorder);
+
 
     $page = new Page;
 
     $page->setTpl('payment', [
-        'order'=>$order->getValues()
+        'order' => $order->getValues()
     ]);
 });
-$app->get('/boleto/:idorder', function($idorder){
+$app->get('/boleto/:idorder', function ($idorder) {
     $order = new Order;
 
-    $order->get((int)$idorder);
-// DADOS DO BOLETO PARA O SEU CLIENTE
-$dias_de_prazo_para_pagamento = 10;
-$taxa_boleto = 5.00;
-$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
-$valor_cobrado = str_replace(".", "", $valor_cobrado);
-$valor_cobrado = str_replace(",", ".",$valor_cobrado);
-$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
-$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
-$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
-$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
-// DADOS DO SEU CLIENTE
-$dadosboleto["sacado"] = $order->getdesperson();
-$dadosboleto["endereco1"] = $order->getdesaddress() . " " . $order->getdesdistrict();
-$dadosboleto["endereco2"] = $order->getdescity() . " - " . $order->getdesstate() . " - " . $order->getdescountry() . " -  CEP: " . $order->getdeszipcode();
-// INFORMACOES PARA O CLIENTE
-$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hcode E-commerce";
-$dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
-$dadosboleto["demonstrativo3"] = "";
-$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
-$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
-$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
-$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
-// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-$dadosboleto["quantidade"] = "";
-$dadosboleto["valor_unitario"] = "";
-$dadosboleto["aceite"] = "";		
-$dadosboleto["especie"] = "R$";
-$dadosboleto["especie_doc"] = "";
-// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
-// DADOS DA SUA CONTA - ITAÚ
-$dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
-$dadosboleto["conta"] = "48781";	// Num da conta, sem digito
-$dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
-// DADOS PERSONALIZADOS - ITAÚ
-$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
-// SEUS DADOS
-$dadosboleto["identificacao"] = "Hcode Treinamentos";
-$dadosboleto["cpf_cnpj"] = "24.700.731/0001-08";
-$dadosboleto["endereco"] = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
-$dadosboleto["cidade_uf"] = "São Bernardo do Campo - SP";
-$dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
-// NÃO ALTERAR!
-$path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
-require_once($path . "funcoes_itau.php");
-require_once($path . "layout_itau.php");
+    $order->get((int) $idorder);
+    // DADOS DO BOLETO PARA O SEU CLIENTE
+    $dias_de_prazo_para_pagamento = 10;
+    $taxa_boleto = 5.00;
+    $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+    $valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = str_replace(".", "", $valor_cobrado);
+    $valor_cobrado = str_replace(",", ".", $valor_cobrado);
+    $valor_boleto = number_format($valor_cobrado + $taxa_boleto, 2, ',', '');
+    $dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
+    $dadosboleto["numero_documento"] = $order->getidorder();    // Num do pedido ou nosso numero
+    $dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+    $dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+    $dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+    $dadosboleto["valor_boleto"] = $valor_boleto;     // Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+    // DADOS DO SEU CLIENTE
+    $dadosboleto["sacado"] = $order->getdesperson();
+    $dadosboleto["endereco1"] = $order->getdesaddress() . " " . $order->getdesdistrict();
+    $dadosboleto["endereco2"] = $order->getdescity() . " - " . $order->getdesstate() . " - " . $order->getdescountry() . " -  CEP: " . $order->getdeszipcode();
+    // INFORMACOES PARA O CLIENTE
+    $dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hcode E-commerce";
+    $dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
+    $dadosboleto["demonstrativo3"] = "";
+    $dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+    $dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+    $dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
+    $dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
+    // DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+    $dadosboleto["quantidade"] = "";
+    $dadosboleto["valor_unitario"] = "";
+    $dadosboleto["aceite"] = "";
+    $dadosboleto["especie"] = "R$";
+    $dadosboleto["especie_doc"] = "";
+    // ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+    // DADOS DA SUA CONTA - ITAÚ
+    $dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
+    $dadosboleto["conta"] = "48781";    // Num da conta, sem digito
+    $dadosboleto["conta_dv"] = "2";     // Digito do Num da conta
+    // DADOS PERSONALIZADOS - ITAÚ
+    $dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+    // SEUS DADOS
+    $dadosboleto["identificacao"] = "Hcode Treinamentos";
+    $dadosboleto["cpf_cnpj"] = "24.700.731/0001-08";
+    $dadosboleto["endereco"] = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
+    $dadosboleto["cidade_uf"] = "São Bernardo do Campo - SP";
+    $dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
+    // NÃO ALTERAR!
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
+    require_once($path . "funcoes_itau.php");
+    require_once($path . "layout_itau.php");
+});
+
+$app->get('/profile/orders', function () {
+    User::verifyLogin();
+
+    $user = User::getFromSession();
+
+    $page = new Page;
+
+    $page->setTpl('profile-orders', [
+        'orders' => $user->getOrders()
+    ]);
+});
+
+$app->get('/profile/orders/:idorder', function ($idorder) {
+    User::verifyLogin();
+
+    $order = new Order;
+
+    $order->get((int) $idorder);
+
+    $cart = new Cart;
+
+    $cart->get((int) $order->getidcart());
+
+    $cart->getCalculateTotal();
+
+    $page = new Page;
+
+    $page->setTpl('profile-orders-detail', [
+        'order' => $order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
+    ]);
 });
